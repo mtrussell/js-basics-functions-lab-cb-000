fe281b077d782fbacbc1d90ec8f537ff52709a70
Matt Trussell <smtrussell@gmail.com>
2018-05-20 02:27:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks() {
-  
+function distanceFromHqInBlocks(street) {
+  if (street < 42) {
+    return 42 - street;
+  } else {
+    return street - 42;
+  }
 }
 
 
 function distanceFromHqInFeet() {
-  
+
 }
 
 
 function distanceTravelledInFeet() {
-  
+
 }
 
 
 function calculatesFarePrice(start, destination) {
-  
+
 }
