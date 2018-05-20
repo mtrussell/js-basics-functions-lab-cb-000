420c6f25e5d7318d1921d63ab549f2d285a64387
Matt Trussell <smtrussell@gmail.com>
2018-05-20 02:57:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // Code your solution in this file!
 
 function distanceFromHqInBlocks(street) {
-  if (street < 42) {
-    return 42 - street;
-  } else {
-    return street - 42;
-  }
+  return street < 42 ? 42 - street : street - 42;
 }
 
 
