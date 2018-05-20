14cbbf1f1ff258839eae6131b3a14c1e3af4abc9
Matt Trussell <smtrussell@gmail.com>
2018-05-20 02:20:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks() {
+  
+}
+
+
+function distanceFromHqInFeet() {
+  
+}
+
+
+function distanceTravelledInFeet() {
+  
+}
+
+
+function calculatesFarePrice(start, destination) {
+  
+}
