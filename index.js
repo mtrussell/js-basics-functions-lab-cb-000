3b6cc7996aa9cc41c7f032697fdf58bc18e0dd86
Matt Trussell <smtrussell@gmail.com>
2018-05-20 02:28:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ function distanceFromHqInBlocks(street) {
 }
 
 
-function distanceFromHqInFeet() {
-
+function distanceFromHqInFeet(street) {
+  const blocks = distanceFromHqInBlocks(street);
+  return blocks * 264;
 }
 
 
